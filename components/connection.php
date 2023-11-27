7108c84cd0507e50d756b95bc600e43d04ec1fd5
Timothy Chin <timothycgy6@gmail.com>
2023-11-27 09:55:04 +0800

    Db Functions working

--- components/connection.php
+++ components/connection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$database = 'sem4test';
+$database = 'test';
 
 $connection = mysqli_connect($host, $user, $password, $database)
     or die('Connection failed. ');
