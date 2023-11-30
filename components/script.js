ea4fd2f1db97a1ea79c7f48b30f9ebf91658cbfc
Huvee <wongrenthou1265@gmail.com>
2023-11-30 22:47:08 +0800

    Login visual

--- components/script.js
+++ components/script.js
@@ -5,15 +5,25 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].classList.remove("fade", "zoom-out");
+    slides[i].style.display = "none";
   }
+  
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("fade", "zoom-out");
+
+  setTimeout(() => {
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex - 1].className += " active";
+    showSlides();
+  }, 10000); // Change image every 2 seconds (adjust with CSS)
 }
