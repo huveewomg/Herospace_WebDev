481883303bed60c224513cd8f92a09105bf6b8e5
Timothy Chin <tp070861@mail.apu.edu.my>
2023-12-09 11:46:23 +0800

    Login and Register working

--- components/login.php
+++ components/login.php
@@ -2,8 +2,8 @@
 include('connection.php');
 session_start();
 
-if (isset($_POST['userid'])) {
-    $userid = $_POST['userid'];
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
     $sql = "SELECT * FROM volunteer";
@@ -11,9 +11,9 @@ if (isset($_POST['userid'])) {
     $found = FALSE;
 
     while ($volunteer = mysqli_fetch_array($result)) {
-        if ($volunteer['volunteerid'] == $userid && $volunteer['password'] == $password) {
+        if ($volunteer['email'] == $email && $volunteer['password'] == $password) {
             $found = TRUE;
-            $_SESSION['username'] = $volunteer['volunteerid'];
+            $_SESSION['email'] = $volunteer['email'];
             $_SESSION['password'] = $volunteer['password'];
             $_SESSION['status'] = 'volunteer';
             break;
@@ -25,9 +25,9 @@ if (isset($_POST['userid'])) {
         $result = mysqli_query($connection, $sql);
 
         while ($charity = mysqli_fetch_array($result)) {
-            if ($charity['charityid'] == $userid && $charity['password'] == $password) {
+            if ($charity['charityid'] == $email && $charity['password'] == $password) {
                 $found = TRUE;
-                $_SESSION['username'] = $charity['charityid'];
+                $_SESSION['email'] = $charity['charityid'];
                 $_SESSION['password'] = $charity['password'];
                 $_SESSION['status'] = 'charity';
                 break;
@@ -41,9 +41,9 @@ if (isset($_POST['userid'])) {
         $result = mysqli_query($connection, $sql);
 
         while ($admin = mysqli_fetch_array($result)) {
-            if ($admin['adminid'] == $userid && $admin['password'] == $password) {
+            if ($admin['adminid'] == $email && $admin['password'] == $password) {
                 $found = TRUE;
-                $_SESSION['username'] = $admin['adminid'];
+                $_SESSION['email'] = $admin['adminid'];
                 $_SESSION['password'] = $admin['password'];
                 $_SESSION['status'] = 'admin';
                 break;
@@ -55,78 +55,167 @@ if (isset($_POST['userid'])) {
         header("Location: home.html");
         echo "alert('Login successful!')";
     } else {
-        echo "<script>alert('Invalid username or password');
+        echo "<script>alert('Invalid email or password');
               window.location = 'login.php'</script>";
     }
 }
+
+elseif(isset($_POST['registerEmail'])) {
+        $registerEmail = $_POST['registerEmail'];
+        $registerName = $_POST['registerName'];
+        $registerPassword = $_POST['registerPassword'];
+        $cpassword = $_POST['cpassword'];
+    
+        if ($registerPassword == $cpassword) {
+            $sql = "INSERT INTO volunteer VALUES ('$registerEmail','$registerPassword','$registerName')";
+            $result = mysqli_query($connection, $sql);
+            if ($result) {
+                echo "<script>alert('Successful registration');
+                      window.location = 'login.php'</script>";
+            } else {
+                echo "<script>alert('Fail registration');
+                      window.location = 'login.php'</script>";
+            }
+        } else {
+            echo "<script>alert(Passwords Do Not Match');
+                  window.location = 'login.php'</script>";
+        }
+    }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Herospace</title>
-    <link rel="stylesheet" href="Loginstyle.css">'
+    <link rel="stylesheet" href="loginstyle.css">'
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
-    
+
 <body>
     <div class="container">
         <div class="slideshow-container">
-            <div class="mySlides fade">
-            <img src="https://cdn.worldofbuzz.com/wp-content/uploads/2023/03/collage-5-2.jpg?strip=all&lossy=1&quality=92&ssl=1">
+            <div class="mySlides">
+                <img src="https://cloudfront-us-east-2.images.arcpublishing.com/reuters/UHEUMBOZ3JNKPDFPX3IEPHOPCI.jpg">
             </div>
-            <div class="mySlides fade"> 
-            <img src="https://media.freemalaysiatoday.com/wp-content/uploads/2023/05/372fa82b-air-kelantan-bernama-100523.jpg">
+            <div class="mySlides">
+                <img src="https://www.undp.org/sites/g/files/zskgke326/files/migration/bb/undp-bb-kalinago-forestday-DSC_8672.jpg">
             </div>
-            <div class="mySlides fade">
-            <img src="https://clips.mstar.com.my/images/blob/02E42499-562B-40DA-89E6-8CDA233298A7">
+            <div class="mySlides">
+                <img src="https://www.cityofcarrollton.com/home/showpublishedimage/25204/637812153581200000">
             </div>
-            
+
             <div style="background: transparent;">
-                <span class="dot"></span> 
-                <span class="dot"></span> 
-                <span class="dot"></span> 
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
             </div>
         </div>
     </div>
 
+    <div class="split"></div>
     <div class="wrapper">
+        <span class="close-icon">
+            close
+        </span>
+
+        <!-- Login Segment -->
         <div class="form-box login">
             <h2>Login</h2>
             <form action="login.php" method="post">
                 <div class="input-box">
                     <span class="material-symbols-outlined">
                         mail
-                        </span>
-                    <input type="text" name="userid" required>
+                    </span>
+                    <input type="text" name="email" required>
                     <label>Email</label>
                 </div>
-                
+
                 <div class="input-box">
                     <span class="material-symbols-outlined">
                         lock
-                        </span>
+                    </span>
                     <input type="password" name="password" required>
                     <label>Password</label>
                 </div>
 
-                <div class="T&C">
+                <div class="TC">
                     <label><input type="checkbox">
-                    I agree to the</label>
+                        I agree to the</label>
                     <a href="#">Term and Conditions</a>
                 </div>
 
                 <button type="submit" class="btn">Login</button>
                 <div class="login-register">
                     <p>New User ?
-                        <a href="/components/vregister.php" class="register-link">Register</a></p>
+                        <a href="#" class="register-link">Register</a>
+                    </p>
                 </div>
+            </form>
         </div>
+
+
+        <!-- Register Segment -->
+        <div class="form-box register">
+            <h2>Registration</h2>
+            <form action="login.php" method="post">
+                <div class="input-box">
+                    <span class="person-icon">
+                        person
+                    </span>
+                    <input type="text" name="registerName"required>
+                    <label>Name</label>
+                </div>
+                <div class="input-box">
+                    <span class="material-symbols-outlined">
+                        mail
+                    </span>
+                    <input type="email" name="registerEmail" required>
+                    <label>Email</label>
+                </div>
+
+                <div class="input-box">
+                    <span class="material-symbols-outlined">
+                        lock
+                    </span>
+                    <input type="password"  name="registerPassword" required>
+                    <label>Password</label>
+                </div>
+
+                <div class="input-box">
+                    <span class="material-symbols-outlined">
+                        lock
+                    </span>
+                    <input type="password"  name="cpassword" required>
+                    <label>Confirm Password</label>
+                </div>
+
+                <div class="message">
+                    <p id="message"></p>
+                </div>
+
+                <div class="T&C">
+                    <label><input type="checkbox">
+                        I agree to the</label>
+                    <a href="#">Term and Conditions</a>
+                </div>
+
+                <button type="submit" onclick="CheckPassword()" class="btn">Register</button>
+                <div class="login-register">
+                    <p>Already have an account ?
+                        <a href="#" class="login-link">Login</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+
     </div>
-        
+
 
 
     <script src="script.js"></script>
 </body>
+
 </html>
