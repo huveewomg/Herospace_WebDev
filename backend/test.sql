25a2471d97a918271b37b4ae41344c524515570f
Timothy Chin <timothycgy6@gmail.com>
2024-02-15 15:21:40 +0800

    Update test.sql

--- backend/test.sql
+++ backend/test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2024 at 11:56 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Generation Time: Feb 15, 2024 at 08:20 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `admins` (
   `adminid` varchar(20) NOT NULL,
   `password` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admins`
@@ -49,7 +49,7 @@ CREATE TABLE `charity` (
   `charityid` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `charity`
@@ -69,7 +69,7 @@ CREATE TABLE `comments` (
   `comment` text DEFAULT NULL,
   `event_id` varchar(255) DEFAULT NULL,
   `volunteer_id` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `comments`
@@ -79,7 +79,8 @@ INSERT INTO `comments` (`comment_id`, `comment`, `event_id`, `volunteer_id`) VAL
 (2, 'Hello World', 'E0', 'chintimothy45@gmail.com'),
 (4, 'Hello World', 'E0', 'chintimothy45@gmail.com'),
 (5, 'Your mother is a whore', 'E0', 'chintimothy45@gmail.com'),
-(6, 'Weeeeee', 'E0', 'chintimothy45@gmail.com');
+(6, 'Weeeeee', 'E0', 'chintimothy45@gmail.com'),
+(7, 'Hello world', 'E2', 'lolman@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -101,17 +102,17 @@ CREATE TABLE `events` (
   `event_updates` text NOT NULL,
   `available` tinyint(1) NOT NULL,
   `start_time` varchar(64) NOT NULL,
-  `end_time` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `end_time` varchar(64) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`event_id`, `event_name`, `event_date`, `charityid`, `event_desc`, `event_req`, `event_fee`, `event_tags`, `signup_link`, `event_state`, `event_updates`, `available`, `start_time`, `end_time`) VALUES
-('E0', 'Testing', '2024-01-03', 'chintimothy45@gmail.com', 'desc', 'req', 90, 'Recreation', 'link', 'Kuala Lumpur', 'dwqdwqd', 0, '12:21am', '12:12am'),
-('E1', 'New Event', '2024-02-08', 'chintimothy45@gmail.com', 'Kedah Event', 'Nothing', 90, 'soup kitchen, cleanup, disaster relief', 'google.com', 'Kedah', 'Hello', 0, '12:00am', '12:00pm'),
-('E2', 'New Event', '2024-02-21', 'chintimothy45@gmail.com', 'We are here', 'Nothing', 100, 'homeless, education, reforestation', 'google.com', 'Johor', '', 0, '12:00pm', '12:00am');
+INSERT INTO `events` (`event_id`, `event_name`, `event_date`, `charityid`, `event_desc`, `event_req`, `event_fee`, `event_tags`, `signup_link`, `event_state`, `event_updates`, `available`, `start_time`, `end_time`, `date_created`) VALUES
+('E1', 'New Event', '2024-02-08', 'chintimothy45@gmail.com', 'Kedah Event', 'Nothing', 90, 'soup kitchen, cleanup, disaster relief', 'google.com', 'Kedah', 'Hello', 0, '12:00am', '12:00pm', '2024-02-19'),
+('E2', 'New Event', '2024-02-21', 'chintimothy45@gmail.com', 'We are here', 'Nothing', 100, 'homeless, education, reforestation', 'google.com', 'Johor', '', 0, '12:00pm', '12:00am', '2024-02-05');
 
 -- --------------------------------------------------------
 
@@ -121,10 +122,19 @@ INSERT INTO `events` (`event_id`, `event_name`, `event_date`, `charityid`, `even
 
 CREATE TABLE `updates` (
   `update_id` int(11) NOT NULL,
-  `update_text` int(11) NOT NULL,
-  `event_id` int(11) NOT NULL,
-  `date` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `update_text` varchar(255) NOT NULL,
+  `event_id` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `updates`
+--
+
+INSERT INTO `updates` (`update_id`, `update_text`, `event_id`, `date`) VALUES
+(4, 'jaden is a maire', 'E0', 'February 6, 2024'),
+(5, 'This is an update', 'E0', 'February 18, 2024'),
+(6, 'This is an update', 'E0', 'February 15, 2024');
 
 -- --------------------------------------------------------
 
@@ -141,16 +151,15 @@ CREATE TABLE `volunteer` (
   `gender` varchar(50) NOT NULL,
   `bio` text NOT NULL,
   `preferences` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `volunteer`
 --
 
 INSERT INTO `volunteer` (`email`, `password`, `name`, `state`, `age`, `gender`, `bio`, `preferences`) VALUES
-('charlie123@gmail.com', 'goodjob', 'Charlie', 'KL', 0, '', 'I love men', ''),
-('chintimothy45@gmail.com', '123456', 'Yong Sheng', 'Kuala Lumpur', 20, 'Male', 'I love walks.', ',dog shelter,reforestation,cleanup,education,animal shelter,clinic,orphanage,river,cat shelter,old folks home'),
-('lolman@gmail.com', '123', '99999', 'KL', 20, 'Male', 'Hello', ''),
+('charlie123@gmail.com', 'goodjob', 'Charlie', 'Kuala Lumpur', 0, '', 'I love men', ''),
+('lolman@gmail.com', '123456', 'Lolman', 'Kuala Lumpur', 20, 'Other', 'This is my biography.', ''),
 ('V001@gmail.com', 'supbro', 'jason', '', 0, '', '', ''),
 ('V002', 'besboi', 'shan', '', 0, '', '', ''),
 ('V003', 'babi', 'peppa babi', '', 0, '', '', '');
@@ -203,13 +212,13 @@ ALTER TABLE `volunteer`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `updates`
 --
 ALTER TABLE `updates`
-  MODIFY `update_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `update_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
