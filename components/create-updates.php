4ce43ceff3726b97574424204b5169a03b5577c6
Timothy Chin <timothycgy6@gmail.com>
2024-02-16 23:31:45 +0800

    Updates Page styling

--- components/create-updates.php
+++ components/create-updates.php
@@ -2,6 +2,10 @@
 session_start();
 require('connection.php');
 
+$event_id = $_GET['event_id'];
+$sql = "SELECT * FROM events WHERE event_id = '$event_id';";
+$result = $connection->query($sql);
+$row = $result->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -11,16 +15,18 @@ require('connection.php');
 <link rel="stylesheet" href="create-event.css" />
 
 <body>
+  <div id="updates-body">
     <form action="save-update.php">
-        <div>Updates</div>
-        <input type="text" name="event_id" value="<?php echo $_GET['event_id']?>" hidden>
-        <textarea id="ev-updates" name="evupdates" cols="30" rows="10" required></textarea><br><br>
-        <input type="submit" value="Submit" id="submit">
+      <div id="updates-title"><?php echo $row['event_name'];?> Updates</div>
+      <input type="text" name="event_id" value="<?php echo $event_id; ?>" hidden>
+      <textarea id="ev-updates" name="evupdates" cols="30" rows="10" placeholder="Update Text..." required></textarea><br><br>
+      <input type="submit" value="Submit" id="submit">
     </form>
+  </div>
 </body>
 <div style="margin-top: 40vh;">
   <!-- Footer Component -->
   <?php include 'footer.php'; ?>
 </div>
-  
+
 </html>
